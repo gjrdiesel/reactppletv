2c15eca3bb8c06b5d58858d6eadcb3f91d383ab9
Justin <knoxxbox@gmail.com>
2021-07-30 23:23:09 -0400

    Mock for real estate

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ function App() {
       <div class="header">
         <div class="topNav">
           <div>Watch Now</div>
-          <div>Share</div>
+          <div>Search Areas</div>
         </div>
       </div>
       <div class="container">
         <div class="callOut">New episodes fridays</div>
-        <h1>Ted Lasso</h1>
+        <h1>Homes in Tampa, FL</h1>
         <div class="callOutDetails">
           Comedy &middot;
           Jul 30, 2021 &middot;
           33min
           Apple TV+
         </div>
-        <button>Play Again</button>
+        <button>View homes</button>
         <div class="episodeDetails">
           S2 E2 &middot; Lavender: Ted is surprised by
           the reappearance of a familiar face. Roy tries out a new gig.
@@ -26,30 +26,32 @@ function App() {
       </div>
       <div class="blackWrapper">
         <div class="episodes">
-          <div class="picker">Season 1</div>
+          <div class="picker">Nearby Neighborhoods</div>
           <div class="scroller">
-            <img alt="Placeholder" class="item" height="150px" src="https://www.tvinsider.com/wp-content/uploads/2021/01/ted-lasso-season-1-ted-rebecca-1-1014x570.jpg" />
-            <img alt="Placeholder" class="item" height="150px" src="https://www.tvinsider.com/wp-content/uploads/2021/01/ted-lasso-season-1-ted-rebecca-1-1014x570.jpg" />
+            <img alt="Placeholder" class="item" height="150px" src="https://images.unsplash.com/photo-1571979622878-622d38ec238c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3198&q=80" />
+            <img alt="Placeholder" class="item" height="150px" src="https://images.unsplash.com/photo-1616113364365-b6013f3dad25?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" />
+            <img alt="Placeholder" class="item" height="150px" src="https://images.unsplash.com/photo-1500021804447-2ca2eaaaabeb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" />
           </div>
         </div>
         <div class="team">
-          <div class="picker">Meet the Team</div>
+          <div class="picker">Agents in the Area</div>
           <div class="scroller">
-            <img alt="Placeholder" class="item" height="150px" src="https://cdn.pocket-lint.com/r/s/1200x/assets/images/156671-tv-news-feature-ted-lasso-release-date-cast-trailers-and-rumours-image1-xvmjstufri.jpg" />
-            <img alt="Placeholder" class="item" height="150px" src="https://media.tenor.com/images/14d399b89c6bb3496d89e1de859eb15b/tenor.png" />
+            <img alt="Placeholder" class="item" height="150px" src="https://images.unsplash.com/photo-1613181013804-1dcba09e6a9d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1022&q=80" />
+            <img alt="Placeholder" class="item" height="150px" src="https://images.unsplash.com/photo-1585240975858-7264fd020798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80" />
+            <img alt="Placeholder" class="item" height="150px" src="https://images.unsplash.com/photo-1614786269829-d24616faf56d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80" />
           </div>
         </div>
         <div class="trailers">
-          <div class="picker">Season 1</div>
+          <div class="picker">Other areas</div>
           <div class="scroller">
-            <img alt="Placeholder" class="item" height="150px" src="https://www.tvinsider.com/wp-content/uploads/2021/01/ted-lasso-season-1-ted-rebecca-1-1014x570.jpg" />
-            <img alt="Placeholder" class="item" height="150px" src="https://www.tvinsider.com/wp-content/uploads/2021/01/ted-lasso-season-1-ted-rebecca-1-1014x570.jpg" />
+            <img alt="Placeholder" class="item" height="150px" src="https://images.unsplash.com/photo-1524429656589-6633a470097c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" />
+            <img alt="Placeholder" class="item" height="150px" src="https://images.unsplash.com/photo-1602868667516-f01b0618a74e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" />
           </div>
         </div>
         <div class="grayDetails">
           <h2>About</h2>
           <div class="card">
-            <h3>Ted Lasso</h3>
+            <h3>More info</h3>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing</p>
           </div>
         </div>
